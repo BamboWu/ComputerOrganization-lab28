92bd4d25a17587ca71486a077290d07c66451fa5
baby-panda <3130102264@zju.edu.cn>
2015-11-28 18:59:08 +0800

    It seems simulation runs normally, while the stall does not stop instruction fetching actually.

--- SRC/InstructionROM.v
+++ SRC/InstructionROM.v
@@ -5,22 +5,35 @@ module InstructionROM(addr,dout);
 	//
 	reg [31 : 0] dout;
 	always @(*)
-		case (addr)
-			6'd0:   dout=32'h0800000b ;
-			6'd1:   dout=32'h20080042 ;
-			6'd2:   dout=32'h20090004 ;
-			6'd3:   dout=32'h01095022 ;
-			6'd4:   dout=32'h01485825 ;
-			6'd5:   dout=32'hac0b000c ;
-			6'd6:   dout=32'h8d2c0008 ;
-			6'd7:   dout=32'h000c4080 ;
-			6'd8:   dout=32'h8d2b0008 ;
-			6'd9:   dout=32'h012a582b ;
-			6'd10:  dout=32'h0800000a ;
-			6'd11:  dout=32'h14000001 ;
-			6'd12:  dout=32'h1000fff4 ;
-			6'd13:  dout=32'h00000000 ;
-			default:dout=32'h00000000 ;
+		case (addr) /* addr = PC/4 */
+			6'd0:   dout=32'h0800000b ; /* op=2h offset=0bh                                        */   
+			                            /* j 0bh # jump to 6'd11, PC = 2ch                         */   
+			6'd1:   dout=32'h20080042 ; /* op=8h rs=0 rt=8 imm=42h                                 */   
+                                        /* addi $t0, $zero, 68           			               */   
+			6'd2:   dout=32'h20090004 ; /* op=8h rs=0 rt=9 imm=4h                                  */   
+			                            /* addi $t1, $zero, 4                                      */   
+			6'd3:   dout=32'h01095022 ; /* op=0h rs=8 rt=9 rd=10 sa=0h func=22h                    */   
+			                            /* sub  $t2, $t0, $t1                                      */   
+			6'd4:   dout=32'h01485825 ; /* op=0h rs=10 rt=8 rd=11 sa=0h func=25h                   */   
+			                            /* or   $t3, $t2, $t0                                      */   
+			6'd5:   dout=32'hac0b000c ; /* op=2bh rs=0 rt=11 offset=0ch                            */   
+			                            /* sw   $t3, 12($zero)                                     */   
+			6'd6:   dout=32'h8d2c0008 ; /* op=23h rs=9 rt=12 offset=0bh                            */   
+			                            /* lw   $t4, 11($t1)                                       */   
+			6'd7:   dout=32'h000c4080 ; /* op=0h rs=0 rt=12 rd=8 sa=2h func=0h                     */   
+			                            /* sll  $t0, $t4, 2                                        */   
+			6'd8:   dout=32'h8d2b0008 ; /* op=23h rs=9 rt=11 offset=0bh                            */   
+			                            /* lw   $t3, 11($t1)                                       */   
+			6'd9:   dout=32'h012a582b ; /* op=0h rs=9 rt=10 rd=11 sa=0h func=2bh                   */   
+			                            /* sltu $t3, $t1, $t2                                      */   
+			6'd10:  dout=32'h0800000a ; /* op=2h offset=0ah                                        */   
+			                            /* j 0ah # jump to itself                                  */   
+			6'd11:  dout=32'h14000001 ; /* op=5h rs=0 rt=0 offset=1                                */   
+			                            /* bne $zero, $zero, 1   # step to next instruction anyway */   
+			6'd12:  dout=32'h1000fff4 ; /* op=4h rs=0 rt=0 offset=-0ch                             */   
+			                            /* beq $zero, $zero, -11 # back to 6b'1                    */   
+			6'd13:  dout=32'h00000000 ; /* nop                                                     */   
+			default:dout=32'h00000000 ; /* nop                                                     */   
 		endcase	
 endmodule
 
--- SRC/MipsPipelineCPU.v
+++ SRC/MipsPipelineCPU.v
@@ -19,12 +19,12 @@
 //
 //////////////////////////////////////////////////////////////////////////////////
 module MipsPipelineCPU(clk, reset, JumpFlag, Instruction_id, ALU_A, 
-                     ALU_B, ALUResult, PC, MemDout_wb,Stall
-						//	,DataTest,ControlTest
-							);
-    input clk;
-    input reset;
-    output[2:0] JumpFlag;
+                       ALU_B, ALUResult, PC, MemDout_wb,Stall
+					   //	,DataTest,ControlTest
+					  );
+    input  clk;
+    input  reset;
+    output [2:0]  JumpFlag;
     output [31:0] Instruction_id;
     output [31:0] ALU_A;
     output [31:0] ALU_B;
@@ -36,133 +36,162 @@ module MipsPipelineCPU(clk, reset, JumpFlag, Instruction_id, ALU_A,
 //  output    ControlTest;
 	 
 //IF  module
-     wire[31:0] Instruction_id;
-	 wire PC_IFWrite,J,JR,Z,IF_flush;
-	 wire[31:0] JumpAddr,JrAddr,BranchAddr,NextPC_if,Instruction_if;
-	 assign JumpFlag={JR,J,Z};
-	 assign IF_flush=Z || J ||JR;
-	
+    wire[31:0] NextPC_if,Instruction_if;
+	/* Feedbacks from ID */
+	wire PC_IFWrite,J,JR,Z;
+	wire[31:0] JumpAddr,JrAddr,BranchAddr;
+	 
 	IF IF(
 //input	
-	 .clk(clk), 
-	 .reset(reset), 
-	 .Z(Z), 
-	 .J(J), 
-	 .JR(JR), 
-	 .PC_IFWrite(PC_IFWrite), 
-	 .JumpAddr(JumpAddr), 
-	 .JrAddr(JrAddr), 
-	 .BranchAddr(BranchAddr), 
-//  output
-	 .Instruction_if(Instruction_if),
-	 .PC(PC),
-	 .NextPC_if(NextPC_if));
+	.clk(clk), 
+	.reset(reset), 
+	.PC_IFWrite(PC_IFWrite), 
+	.J(J), 
+	.JR(JR), 
+	.Z(Z), 
+	.JumpAddr(JumpAddr), 
+	.JrAddr(JrAddr), 
+	.BranchAddr(BranchAddr), 
+//output
+	.PC(PC),  /* a probe, defined at very begining */
+	.NextPC_if(NextPC_if),
+	.Instruction_if(Instruction_if));
+	 
+	/* a probe, defined at the very beginning */
+	assign JumpFlag={JR,J,Z};
  
-//   IF->ID Register
-
-     wire[31:0] NextPC_id;
-     dffr #(.WIDTH(64)) IFdffID(.clk(clk&PC_IFWrite),.r(reset|IF_flush),
-	                            .d({NextPC_if,Instruction_if}),
-						        .q({NextPC_id,Instruction_id}));
-
+//IF->ID Register
+    wire[31:0] NextPC_id,Instruction_id;
+    /* Combined feedbacks from ID */
+    wire IF_flush;
+	assign IF_flush=Z || J ||JR;
+    
+    dffr #(.WIDTH(64)) IF2ID(.clk(clk&PC_IFWrite),.r(reset|IF_flush),
+	       /*32+32*/         .d({NextPC_if,Instruction_if}),
+						     .q({NextPC_id,Instruction_id}));
      
-//  ID Module	
-    wire[4:0] RtAddr_id,RdAddr_id,RsAddr_id;
-    wire  RegWrite_wb,MemRead_ex,MemtoReg_id,RegWrite_id,MemWrite_id;
-    wire  MemRead_id,ALUSrcA_id,ALUSrcB_id,RegDst_id,stall;
-    wire[4:0]  RegWriteAddr_wb,RegWriteAddr_ex,ALUCode_id;
-    wire[31:0] RegWriteData_wb,Imm_id,Sa_id,RsData_id,RtData_id;
-    ID  ID (
-	    .clk(clk),
-		.Instruction_id(Instruction_id), 
-		.NextPC_id(NextPC_id), 
-		.RegWrite_wb(RegWrite_wb), 
-		.RegWriteAddr_wb(RegWriteAddr_wb), 
-		.RegWriteData_wb(RegWriteData_wb), 
-		.MemRead_ex(MemRead_ex), 
-        .RegWriteAddr_ex(RegWriteAddr_ex), 
-		.MemtoReg_id(MemtoReg_id), 
-		.RegWrite_id(RegWrite_id), 
-		.MemWrite_id(MemWrite_id), 
-		.MemRead_id(MemRead_id), 
-		.ALUCode_id(ALUCode_id), 
-		.ALUSrcA_id(ALUSrcA_id), 
-		.ALUSrcB_id(ALUSrcB_id), 
-		.RegDst_id(RegDst_id), 
-		.Stall(Stall), 
-		.Z(Z), 
-		.J(J), 
-		.JR(JR), 
-		.PC_IFWrite(PC_IFWrite),  
-		.BranchAddr(BranchAddr), 
-		.JumpAddr(JumpAddr),
-		.JrAddr(JrAddr),
-		.Imm_id(Imm_id), 
-		.Sa_id(Sa_id), 
-		.RsData_id(RsData_id), 
-		.RtData_id(RtData_id),
-		.RtAddr_id(RtAddr_id),
-		.RdAddr_id(RdAddr_id),
-		.RsAddr_id(RsAddr_id));
+//ID Module	
+    wire  MemToReg_id,RegWrite_id,MemWrite_id,MemRead_id,ALUSrcA_id,ALUSrcB_id,RegDst_id;
+    wire[31:0] Imm_id,Sa_id,RsData_id,RtData_id;
+    wire[4:0] ALUCode_id,RtAddr_id,RdAddr_id,RsAddr_id;
+    /* Feedbacks from MEM2WB*/
+    wire  RegWrite_wb;
+    wire[4:0]  RegWriteAddr_wb;
+    wire[31:0] RegWriteData_wb;
+	/* Feedback from ID2EX */
+    wire  MemRead_ex;
+    /* Feedbacks from EX*/
+    wire[4:0]  RegWriteAddr_ex;
+	
+    ID ID(
+	.clk(clk),
+	.NextPC_id(NextPC_id), 
+	.Instruction_id(Instruction_id), 
+	.RegWrite_wb(RegWrite_wb), 
+	.RegWriteAddr_wb(RegWriteAddr_wb), 
+	.RegWriteData_wb(RegWriteData_wb), 
+	.MemRead_ex(MemRead_ex), 
+    .RegWriteAddr_ex(RegWriteAddr_ex), 
+    /*output*/
+	.MemtoReg_id(MemToReg_id), 
+	.RegWrite_id(RegWrite_id), 
+	.MemWrite_id(MemWrite_id), 
+	.MemRead_id(MemRead_id), 
+	.ALUCode_id(ALUCode_id), 
+	.ALUSrcA_id(ALUSrcA_id), 
+	.ALUSrcB_id(ALUSrcB_id), 
+	.RegDst_id(RegDst_id), 
+	.Stall(Stall),/* also work as a probe, defined at the very beginning */
+	.Z(Z), 
+	.J(J), 
+	.JR(JR), 
+	.PC_IFWrite(PC_IFWrite),  
+	.BranchAddr(BranchAddr), 
+	.JumpAddr(JumpAddr),
+	.JrAddr(JrAddr),
+	.Imm_id(Imm_id), 
+	.Sa_id(Sa_id), 
+	.RsData_id(RsData_id), 
+	.RtData_id(RtData_id),
+	.RtAddr_id(RtAddr_id),
+	.RdAddr_id(RdAddr_id),
+	.RsAddr_id(RsAddr_id));
 
-//   ID->EX  Register
+//ID->EX  Registers
 
-	 wire[4:0] RdAddr_ex,RsAddr_ex,RtAddr_ex,ALUCode_ex;
-     wire MemtoReg_ex,RegWrite_ex,MemWrite_ex;
-	 wire[31:0] Sa_ex,Imm_ex,RsData_ex,RtData_ex;
-	 dffr DFF(.clk(clk),.r(reset),.d(MemRead_id),.q(MemRead_ex));
-     dffr #(.WIDTH(155)) IDdffEX(.clk(clk),.r(reset|Stall),
-    	                         .d({MemtoReg_id,RegWrite_id, MemWrite_id,/*MemRead_id, /*1+1+1+1*/
-    							     ALUCode_id,ALUSrcA_id,ALUSrcB_id,RegDst_id, /*5+1+1+1*/ /*32+32+5+5+5+32+32*/
-    							     Sa_id,Imm_id,RdAddr_id,RsAddr_id,RtAddr_id,RsData_id,RtData_id}),
-    						     .q({MemtoReg_ex,RegWrite_ex,MemWrite_ex,/*MemRead_ex,*/
-    							     ALUCode_ex,ALUSrcA_ex,ALUSrcB_ex,RegDst_ex,
-    							     Sa_ex,Imm_ex,RdAddr_ex,RsAddr_ex,RtAddr_ex,RsData_ex,RtData_ex}));
+    wire MemToReg_ex,RegWrite_ex;
+    wire MemWrite_ex; /*MemRead_ex feedback to ID*/
+	wire[4:0] ALUCode_ex;
+	wire ALUSrcA_ex,ALUSrcB_ex,RegDst_ex;
+	wire[31:0] Sa_ex,Imm_ex,RsData_ex,RtData_ex;
+	wire[4:0] RdAddr_ex,RsAddr_ex,RtAddr_ex;
+	 
+	dffr #(.WIDTH(2))   ID2EX_wb(.clk(clk),.r(reset|Stall),
+	       /*1+1*/      /* WB */ .d({MemToReg_id,RegWrite_id}),
+	    				  		 .q({MemToReg_ex,RegWrite_ex}));
+	dffr #(.WIDTH(2))   ID2EX_mem(.clk(clk),.r(reset|Stall),
+	       /*1+1*/      /* M */   .d({MemWrite_id,MemRead_id}),
+						  		  .q({MemWrite_ex,MemRead_ex}));
+	dffr #(.WIDTH(8))   ID2EX_ex(.clk(clk),.r(reset|Stall),
+	       /*5+1+1+1*/  /* EX */ .d({ALUCode_id,ALUSrcA_id,ALUSrcB_id,RegDst_id}),
+								 .q({ALUCode_ex,ALUSrcA_ex,ALUSrcB_ex,RegDst_ex}));
+	dffr #(.WIDTH(143)) ID2EX_Data(.clk(clk),.r(reset|Stall),
+	       /*32*2+5*3+32*2*/       .d({Sa_id,Imm_id,RdAddr_id,RsAddr_id,RtAddr_id,RsData_id,RtData_id}),
+								   .q({Sa_ex,Imm_ex,RdAddr_ex,RsAddr_ex,RtAddr_ex,RsData_ex,RtData_ex}));
 
+// EX Module
+    /*wire[4:0] RegWriteAddr_ex has feedback to ID */
+    wire[31:0] ALUResult_ex,MemWriteData_ex;
+    /* RegWriteAddr_wb,RegWriteData_wb feedback from WB defined at ID stage*/
+    /* Feedback from MEM */
+    wire RegWrite_mem;
+    wire[4:0] RegWriteAddr_mem;
+    wire[31:0] ALUResult_mem;
+ 
+    EX EX(
+    .ALUCode_ex(ALUCode_ex), 
+    .ALUSrcA_ex(ALUSrcA_ex), 
+    .ALUSrcB_ex(ALUSrcB_ex), 
+    .RegDst_ex(RegDst_ex), 
+    .Sa_ex(Sa_ex), 
+    .Imm_ex(Imm_ex), 
+    .RdAddr_ex(RdAddr_ex),
+    .RsAddr_ex(RsAddr_ex), 
+    .RtAddr_ex(RtAddr_ex), 
+    .RsData_ex(RsData_ex), 
+    .RtData_ex(RtData_ex), 
+    .RegWrite_wb(RegWrite_wb), 
+    .RegWriteAddr_wb(RegWriteAddr_wb), 
+    .RegWriteData_wb(RegWriteData_wb), 
+    .RegWrite_mem(RegWrite_mem), 
+    .RegWriteAddr_mem(RegWriteAddr_mem), 
+    .ALUResult_mem(ALUResult_mem), 
+    /*output*/
+    .RegWriteAddr_ex(RegWriteAddr_ex), 
+    .ALUResult_ex(ALUResult_ex), 
+    .MemWriteData_ex(MemWriteData_ex), 
+    .ALU_A(ALU_A), /* two probes, defined at very beginning */
+    .ALU_B(ALU_B));
 
-
-// EX Module	 
- wire[31:0] ALUResult_mem,ALUResult_ex,MemWriteData_ex;
- wire[4:0] RegWriteAddr_mem;
- wire RegWrite_mem;
- EX  EX(
- .RegDst_ex(RegDst_ex), 
- .ALUCode_ex(ALUCode_ex), 
- .ALUSrcA_ex(ALUSrcA_ex), 
- .ALUSrcB_ex(ALUSrcB_ex), 
- .Imm_ex(Imm_ex), 
- .Sa_ex(Sa_ex), 
- .RsAddr_ex(RsAddr_ex), 
- .RtAddr_ex(RtAddr_ex), 
- .RdAddr_ex(RdAddr_ex),
- .RsData_ex(RsData_ex), 
- .RtData_ex(RtData_ex), 
- .RegWriteData_wb(RegWriteData_wb), 
- .ALUResult_mem(ALUResult_mem), 
- .RegWriteAddr_wb(RegWriteAddr_wb), 
- .RegWriteAddr_mem(RegWriteAddr_mem), 
- .RegWrite_wb(RegWrite_wb), 
- .RegWrite_mem(RegWrite_mem), 
- .RegWriteAddr_ex(RegWriteAddr_ex), 
- .ALUResult_ex(ALUResult_ex), 
- .MemWriteData_ex(MemWriteData_ex), 
- .ALU_A(ALU_A), 
- .ALU_B(ALU_B));
-
-assign ALUResult=ALUResult_ex;
+    assign ALUResult=ALUResult_ex;
+    /* a probe, defined at the very beginning */
 
 //EX->MEM
-     wire[31:0] MemWriteData_mem;
-     wire MemtoReg_mem;
-     dffr #(.WIDTH(72)) EXdffMEM(.clk(clk),.r(reset),
-	                             .d({MemtoReg_ex,RegWrite_ex,MemWrite_ex, /*1+1+1+1*/
-			 					    ALUResult_ex,MemWriteData_ex,RegWriteAddr_ex}), /*32+32+5*/
-			    				 .q({MemtoReg_mem,RegWrite_mem,MemWrite_mem,
-			 					    ALUResult_mem,MemWriteData_mem,RegWriteAddr_mem}));
 
+    wire MemToReg_mem; /* RegWrite_mem feedbacks to EX */
+	wire MemWrite_mem;
+    wire[31:0] MemWriteData_mem;
+	/* ALUResult_mem,RegWriteAddr_mem feedback to EX */
+	dffr #(.WIDTH(2))  EX2MEM_wb(.clk(clk),.r(reset),
+	       /*1+1*/     /* WB */  .d({MemToReg_ex,RegWrite_ex}),
+		                         .q({MemToReg_mem,RegWrite_mem}));
+	dffr   /* M */     EX2MEM_mem(.clk(clk),.r(reset),.d(MemWrite_ex),.q(MemWrite_mem));
+	dffr #(.WIDTH(69)) EX2MEM_Data(.clk(clk),.r(reset),
+	       /*32+32+5*/ /* Data */  .d({ALUResult_ex,MemWriteData_ex,RegWriteAddr_ex}),
+								   .q({ALUResult_mem,MemWriteData_mem,RegWriteAddr_mem}));
 
 //MEM Module
-	DataRAM   DataRAM(
+	DataRAM DataRAM(
 	.addr(ALUResult_mem[7:2]),
 	.clk(clk),
 	.din(MemWriteData_mem),
@@ -170,15 +199,16 @@ assign ALUResult=ALUResult_ex;
 	.we(MemWrite_mem));
 
 //MEM->WB
-  
-    dffr #(.WIDTH(39)) MEMdffWB(.clk(clk),.r(reset),
-	                            .d({MemtoReg_mem,RegWrite_mem,       /*1+1*/
-							        ALUResult_mem,RegWriteAddr_mem}),/*32+5*/
-							    .q({MemToReg_wb,RegWrite_wb,
-							        ALUResult_wb,RegWriteAddr_wb}));
+    wire MemToReg_wb; /* RegWrite_wb feedbacks to ID */
+	wire[31:0] ALUResult_wb; /* RegWriteAddr_wb feedbacks to ID */
+    dffr #(.WIDTH(2))  MEM2WB_wb(.clk(clk),.r(reset),
+	       /*1+1*/     /* WB */  .d({MemToReg_mem,RegWrite_mem}),
+					     	     .q({MemToReg_wb,RegWrite_wb}));
+	dffr #(.WIDTH(37)) MEM2WB_Data(.clk(clk),.r(reset),
+		   /*32+5*/   /* Data */   .d({ALUResult_mem,RegWriteAddr_mem}),
+							       .q({ALUResult_wb,RegWriteAddr_wb}));
 
 //WB
   assign RegWriteData_wb=MemToReg_wb?MemDout_wb:ALUResult_wb;
-
 
 endmodule
